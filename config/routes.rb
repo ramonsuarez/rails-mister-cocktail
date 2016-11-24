c25885acd501794849ba06cac031bf6818b0f7ac
Ramon Suarez <ramon@betacowork.com>
2016-11-24 14:58:29 +0100

    Models dones, controllers and views started, problem with doses routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :cocktails, only: [:create, :index, :new, :show]
+  resources :doses, only: [:new, :create, :destroy]
+  resources :ingredients, only: [:create, :index, :new, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
